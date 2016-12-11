10576cca87ecfa8e71e91fb850a3de791d963160
WilliamFisher <ashton0312@gmail.com>
2016-12-11 00:33:24 -0500

    Init

---
+++ Assets/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public float slowness = 10f;
+	public int score = 0;
+	public GameObject gameOverPanel;
+	public Text score2Text;
+
+	private Text scoreText;
+	private int realScore;
+
+	void Start()
+	{
+		scoreText = GameObject.Find ("Score").GetComponent<Text> ();
+	}
+
+	public void EndGame()
+	{
+		gameOverPanel.SetActive (true);
+		score2Text.text = realScore.ToString ();
+		StartCoroutine (RestartLevel ());
+	}
+
+	IEnumerator RestartLevel()
+	{
+		Time.timeScale = 1f / slowness;
+		Time.fixedDeltaTime = Time.fixedDeltaTime / slowness;
+
+		yield return new WaitForSeconds (2f / slowness);
+
+		Time.timeScale = 1f;
+		Time.fixedDeltaTime = Time.fixedDeltaTime * slowness;
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+	}
+
+	public void IncreaseScore()
+	{
+		score++;
+		realScore = score / 4;
+		scoreText.text = realScore.ToString ();
+	}
+}
---
+++ Assets/MenuManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+	public GameObject music;
+
+	public void StartGame()
+	{
+		SceneManager.LoadScene (1);
+	}
+
+	public void Awake()
+	{
+		music = GameObject.Find ("Music");
+		DontDestroyOnLoad (music);
+	}
+}
